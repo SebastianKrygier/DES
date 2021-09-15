128ff06d6b58b5627f390a10524b0c3e163c492e
Sebastian Krygier <sebastian.krygier@outlook.com>
2021-09-15 14:18:05 +0200

    project

---
+++ cmake-build-debug/CMakeFiles/DES.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DES.dir/main.cpp.obj"
+  "DES.exe"
+  "DES.exe.manifest"
+  "DES.lib"
+  "DES.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DES.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
